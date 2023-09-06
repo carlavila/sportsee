64d45a07548e7bef85ba96823094754999daca0c
carla <76110813+carlavila@users.noreply.github.com>
2023-09-06 10:41:50 +0200

    routes + pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Error from './pages/Error';
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route exact path='/user/:id' element={<Home />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
